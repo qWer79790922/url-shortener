333fec11085c9517fd360d36d76eee9f97af4d86
Min <qWer79790922@gmail.com>
2025-06-24 11:33:32 +0800

    chore(admin): register ShortURL model in Django admin

--- page/admin.py
+++ page/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import ShortURL
 
-# Register your models here.
+@admin.register(ShortURL)
+class ShortURLAdmin(admin.ModelAdmin):
+    list_display = ['short_code', 'original_url', 'is_protected', 'created_at']
+    search_fields = ['short_code', 'original_url']
+    list_filter = ['is_protected', 'created_at']
